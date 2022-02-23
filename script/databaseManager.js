8a5a29da06db26c3705c4a2219b02b879ffeebae
Ayden <aydendiel@gmail.com>
2022-02-22 19:51:43 -0500

    changed some package.json stuff

---
+++ script/databaseManager.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+/**
+ * Handles all mongoDB database stuff.
+ */
+class DatabaseManager{
+    constructor(){
+
+    }
+}
+
+module.exports = DatabaseManager;
